d2ef224eea1746ec92e91957fedd2ddc28e228eb
José Antonio González Morente <jgonzalezmorente@gmail.com>
2021-02-11 20:22:52 +0100

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,34 @@
+import bodyParser from 'body-parser';
+
+import Server from './classes/server';
+import router from './routes/router';
+import cors from 'cors';
+
+
+const server = new Server();
+
+
+// Bodyparser
+
+server.app.use( 
+    bodyParser.urlencoded( {
+        extended: true
+    })
+);
+
+server.app.use(
+    bodyParser.json()
+);
+
+// CORS
+server.app.use( cors( { origin: true, credentials: true }));
+
+// Rutas de servicios
+
+server.app.use('/', router );
+
+
+server.start( 
+    () => console.log(`Servidor corriendo en el puerto ${ server.port }`)
+);
+
