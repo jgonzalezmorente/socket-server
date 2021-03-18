79c897a4ed4b13d2d685dfd09032573325303a45
José Antonio González Morente <jgonzalezmorente@gmail.com>
2021-03-18 17:47:50 +0100

    Fin sección 5

--- routes/router.ts
+++ routes/router.ts
@@ -1,6 +1,6 @@
-
-
 import { Router, Request, Response } from 'express';
+
+import Server from '../classes/server';
 
 
 const router = Router();
@@ -17,9 +17,18 @@ router.get( '/mensajes', ( req: Request, res: Response ) => {
 
 router.post( '/mensajes', ( req: Request, res: Response ) => {
 
-    const cuerpo = req.body.cuerpo;
     const de     = req.body.de;
+    const cuerpo = req.body.cuerpo;
 
+    const payload = {
+        de,
+        cuerpo
+    };
+    
+    const server = Server.instance;
+
+    server.io.emit( 'mensaje-nuevo', payload );
+    
     res.json({
         ok: true,
         cuerpo,
@@ -31,18 +40,28 @@ router.post( '/mensajes', ( req: Request, res: Response ) => {
 
 router.post( '/mensajes/:id', ( req: Request, res: Response ) => {
 
-    const cuerpo = req.body.cuerpo;
-    const de     = req.body.de;
     const id     = req.params.id;
+    const de     = req.body.de;
+    const cuerpo = req.body.cuerpo;
+
+    const payload = {
+        de,
+        cuerpo
+    };
+
+    const server = Server.instance;
+
+    server.io.in( id ).emit( 'mensaje-privado', payload );
 
     res.json({
         ok: true,
-        cuerpo,
+        id,
         de,
-        id
+        cuerpo
     });
 
 });
+
 
 
 export default router;
--- sockets/sockets.ts
+++ sockets/sockets.ts
@@ -1,10 +1,45 @@
 import { Socket } from 'socket.io';
 import socketIO from 'socket.io';
+import { UsuariosLista } from '../classes/usuarios-lista';
+import { Usuario } from '../classes/usuario';
+
+
+
+
+export const usuariosConectados = new UsuariosLista();
+
+export const conectarCliente = ( cliente: Socket ) => {
+
+    const usuario = new Usuario( cliente.id );
+    usuariosConectados.agregar( usuario );
+};
+
+
 
 export const desconectar = ( cliente: Socket ) => {
+    
+    cliente.on( 'disconnect', () => {
+        console.log( 'Cliente desconectado' );
+        usuariosConectados.borrarUsuario( cliente.id );
+    } );
 
-    cliente.on( 'disconnect', () => console.log( 'Cliente desconectado' ) );
+}
 
+
+
+// Configurar usuario
+export const configurarUsuario = ( cliente: Socket, io: socketIO.Server ) => {
+
+    cliente.on( 'configurar-usuario', ( payload: { nombre: string }, callback: ( resp: { ok: boolean, mensaje: string } ) => void ) => {        
+
+        usuariosConectados.actualizarNombre( cliente.id, payload.nombre );
+
+        callback( {
+            ok: true,
+            mensaje: `Usuario  ${ payload.nombre } configurado `
+        });
+
+    });
 }
 
 
@@ -21,3 +56,6 @@ export const mensaje = ( cliente: Socket, io: socketIO.Server ) => {
 
     }) ;
 }
+
+
+
